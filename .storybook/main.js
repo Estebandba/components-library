53afd530fff5d7b743c9c0a35d582f6e30535882
Esteban Blanco <estebandba20@gmail.com>
2021-05-17 20:41:58 +0100

    feat: added setup for storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,12 +7,10 @@ module.exports = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-actions",
-    "@storybook/addon-docs",
-    {
-      name: "@storybook/addon-docs",
-      options: {
-        configureJSX: true
-      }
-    }
-  ]
+    "@storybook/addon-toolbars",
+    "@storybook/addon-knobs",
+  ],
+  "core": {
+    "builder": "webpack5"
+  }
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,41 @@
+import React from 'react';
+
+import { ThemeProvider } from 'styled-components';
+import {defaultTheme, darkTheme} from '../src/utils'
+import { withKnobs } from '@storybook/addon-knobs';
+
+const MyThemes = {
+  defaultTheme,
+  darkTheme
+}
+
+export const globalTypes = {
+  theme: {
+    name: 'Theme',
+    description: 'Global theme for components',
+    defaultValue: 'defaultTheme',
+    toolbar: {
+      icon: 'circlehollow',
+      // Array of plain string values or MenuItem shape (see below)
+      items: ['defaultTheme', 'darkTheme'],
+    },
+  },
+};
+
+// Function to obtain the intended theme
+const getTheme = (themeName) => {
+  return MyThemes[themeName]
+}
+
+const withThemeProvider=(Story,context)=>{
+  const theme = getTheme(context.globals.theme);
+  return (
+    <ThemeProvider theme={theme}>
+      <Story {...context} />
+    </ThemeProvider>
+  )
+}
+export const decorators = [withThemeProvider, withKnobs];
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
